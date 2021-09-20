6afde0973d607ade287c664ef9d0b471a1a1f1c2
Konstantin Parashchevin <kpstsp@gmail.com>
2021-09-21 01:03:47 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Demo app with using React admin framework
+
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { Admin, Resource } from 'react-admin';
+import { UserList }  from './users';
+import { PostList, PostEdit, PostCreate }  from './posts';
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+// const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={ PostList } edit={ PostEdit } create={ PostCreate } />
+    <Resource name="users" list={ UserList } />
+  </Admin>
+);
+
+export default App;
